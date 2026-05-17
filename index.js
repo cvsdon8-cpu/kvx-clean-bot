4501b8dbc4a62aa5ea1cb41d081e793f913a7d87
cvsdon8-cpu <cvsdon8@gmail.com>
2026-05-18 01:31:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
-const admin = require('firebase-admin');
 const express = require('express');
 
 const app = express();
 app.use(express.json());
-
-const serviceAccount = require('./firebase.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://kryvex-system-default-rtdb.asia-southeast1.firebasedatabase.app/"
-});
-
-const db = admin.database();
 
 const token = "8532645384:AAE1EPd4Ol51amuh49f6G-ZO9wbkeptrPvc";
 
@@ -20,9 +10,7 @@ const bot = new TelegramBot(token);
 
 const url = "https://kvx-clean-bot-production.up.railway.app";
 
-bot.setWebHook(`${url}/bot${token}`)
-  .then(() => console.log("Webhook set!"))
-  .catch(console.error);
+bot.setWebHook(`${url}/bot${token}`);
 
 app.post(`/bot${token}`, async (req, res) => {
   const msg = req.body.message;
@@ -30,12 +18,6 @@ app.post(`/bot${token}`, async (req, res) => {
   if (msg) {
     const chatId = msg.chat.id;
     const text = msg.text || "";
-
-    await db.ref("messages").push({
-      user: msg.from.username || "unknown",
-      text,
-      time: Date.now()
-    });
 
     bot.sendMessage(chatId, `Saved: ${text}`);
   }
