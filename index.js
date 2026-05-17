b3dc9518336d9ea4d6488894f07f2c975cf69576
cvsdon8-cpu <cvsdon8@gmail.com>
2026-05-18 01:21:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ const token = "8532645384:AAE1EPd4Ol51amuh49f6G-ZO9wbkeptrPvc";
 
 const bot = new TelegramBot(token);
 
-const url = "kvx-clean-bot-production.up.railway.app";
+const url = "https://kvx-clean-bot-production.up.railway.app";
 
-bot.setWebHook(`${url}/bot${token}`);
+bot.setWebHook(`${url}/bot${token}`)
+  .then(() => console.log("Webhook set!"))
+  .catch(console.error);
 
 app.post(`/bot${token}`, async (req, res) => {
   const msg = req.body.message;
