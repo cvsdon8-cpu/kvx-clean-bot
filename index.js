e57554073651544ec60be8836711a55dc2cf0a63
cvsdon8-cpu <cvsdon8@gmail.com>
2026-05-17 23:56:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ admin.initializeApp({
 
 const db = admin.database();
 
-const token = process.env.8532645384:AAF8-2fnLx6yJpmMaQFtF0lN1cLpCb-2F9g;
+const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
